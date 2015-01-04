d5ed8b8e37847cf7fcb22d66a44b9a121ff2dc34
Matt Dubord <matt@screenplay.com>
2015-01-04 07:58:23 -0800

    cleanup

--- src/main/java/org/sevenasix/medium/repo/Action.java
+++
@@ -1,5 +0,0 @@
-package org.willowlms.repo;
-
-public enum Action {
-	STORE, DELETE, COPY, RENAME, MOVE, GET_ATTRIBS, CHOWN, MODIFY;
-}
--- src/main/java/org/sevenasix/medium/roles/Role.java
+++
@@ -1,4 +0,0 @@
-package org.willowlms.roles;
-
-public interface Role {
-}
