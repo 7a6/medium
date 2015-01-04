c796a9472a4994f97942fef6b5d8588da17770da
Matt Dubord <mttdbrd@gmail.com>
2015-01-04 15:03:33 -0800

    Modified directory structure

---
+++ src/main/java/org/sevenasix/medium/actors/Student.java
@@ -0,0 +1,16 @@
+package org.sevenasix.medium.actors;
+
+public class Student {
+
+    private final String name;
+
+
+    public Student(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+}
