9d2b31109b8bbf7b3722f1b05f1b35338df5c4ac
Matt Dubord <matt@screenplay.com>
2015-07-04 10:51:58 -0700

    initial test to get project started

--- src/test/java/org/sevenasix/medium/HomeControllerTest.java
+++ src/test/java/org/sevenasix/medium/HomeControllerTest.java
@@ -1,4 +1,21 @@
 package org.sevenasix.medium;
 
+import org.junit.Test;
+import org.sevenasix.medium.web.HomeController;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
 public class HomeControllerTest {
+
+    @Test
+    public void verifyHomeControllerView() throws Exception {
+
+        HomeController controller = new HomeController();
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+        mockMvc.perform(MockMvcRequestBuilders.get("/")).andExpect(view().name("home"));
+    }
+
 }
