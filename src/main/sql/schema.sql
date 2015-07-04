7b8f769affdb0a460f953e5d618eb1b5074d014e
Matt Dubord <matt@screenplay.com>
2015-07-04 10:04:49 -0700

    resources files added

--- src/main/sql/schema.sql
+++ src/main/sql/schema.sql
@@ -1,1 +1,10 @@
 CREATE DATABASE IF NOT EXISTS medium CHARACTER SET = 'utf8' COLLATE = 'utf8_unicode_ci';
+
+CREATE TABLE IF NOT EXISTS user 
+(   "id" INT NOT NULL AUTO_INCREMENT,
+    "student_name" VARCHAR(255) NOT NULL,
+    "short_name" VARCHAR(25),
+    "username" VARCHAR(255) NOT NULL,
+    "password" VARCHAR(255) NOT NULL,
+    "auth" 
+
