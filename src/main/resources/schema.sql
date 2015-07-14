ff4fc4f73c238c3d3f1995dda2e1784226056dfe
Matt Dubord <mttdbrd@gmail.com>
2015-07-14 06:56:54 -0700

    work on data config

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,10 +1,10 @@
 CREATE DATABASE IF NOT EXISTS medium CHARACTER SET = 'utf8' COLLATE = 'utf8_unicode_ci';
 
 CREATE TABLE IF NOT EXISTS user 
-(   "id" INT NOT NULL AUTO_INCREMENT,
-    "long_name" VARCHAR(255) NOT NULL,
-    "short_name" VARCHAR(25),
-    "username" VARCHAR(255) NOT NULL,
-    "password" VARCHAR(255) NOT NULL,
-    "auth" 
+(   id INT AUTO_INCREMENT PRIMARY KEY,
+    long_name VARCHAR(255) NOT NULL,
+    short_name VARCHAR(25),
+    username VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
+)
 
