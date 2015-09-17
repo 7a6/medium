59035b949b243226a5ac47900dc79fefd2864c70
Matt Dubord <matt@screenplay.com>
2015-09-16 21:43:48 -0700

    merge

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,10 +1,9 @@
 CREATE DATABASE IF NOT EXISTS medium CHARACTER SET = 'utf8' COLLATE = 'utf8_unicode_ci';
 
 CREATE TABLE IF NOT EXISTS user 
-(   "id" INT NOT NULL AUTO_INCREMENT,
+(   "username" VARCHAR(255) NOT NULL PRIMARY KEY,
     "long_name" VARCHAR(255) NOT NULL,
     "short_name" VARCHAR(25),
-    "username" VARCHAR(255) NOT NULL,
     "password" VARCHAR(255) NOT NULL,
     "auth" 
 
