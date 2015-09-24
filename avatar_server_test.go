91994a0d34d87add28d8e491c80dc268c091957a
Matt Dubord <matt@screenplay.com>
2015-09-24 07:39:09 -0700

    added first ginkgo test

--- avatar_server_test.go
+++ avatar_server_test.go
@@ -3,22 +3,33 @@ package main_test
 import (
 	. "github.com/7a6/medium"
 	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
 	"net/http"
 	"net/http/httptest"
 )
 
 var _ = Describe("Server", func() {
 
-	var server http.Server
+	var server *httptest.Server
+	var recorder *httptest.ResponseRecorder
+	var request *http.Request
 
 	BeforeEach(func() {
-
-		server = httptest.NewServer(HandleAvatar)
-
+		server = httptest.NewServer(http.HandlerFunc(HandleAvatar))
+		server.URL = "192.168.1.2:8080"
+		defer server.Close()
 	})
-	Context("when the user has an avatar", func() {
-		It("can serve an avatar", func() {
-			//	Expect
+
+	Describe("GET /avatar", func() {
+		BeforeEach(func() {
+			recorder = httptest.NewRecorder()
+
+		})
+		Context("when the user has an avatar", func() {
+			It("can serve the avatar", func() {
+				request, _ = http.NewRequest("GET", "/avatar/", nil)
+				Expect(recorder.Code).To(Equal(200))
+			})
 		})
 	})
 })
